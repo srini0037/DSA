b34abc11f720a44b4dfe1c52f6ed91e5def1f723
Srinivas Santhanathan <srinivass@Srinivass-MacBook-Pro.local>
2026-04-30 23:36:02 +0100

    Pattern Day3

---
+++ PatternSolving/pattern1.py
@@ -0,0 +1,6 @@
+num = input("Enter a number: ")
+
+for i in range(1, int(num)+1):
+    for j in range(1, int(num)+1):
+        print("*", end=" ")
+    print("")
---
+++ PatternSolving/pattern2.py
@@ -0,0 +1,7 @@
+num = input("Enter a number: ")
+
+for i in range(1, int(num)+1):
+    for j in range(1, int(num)+1):
+        print(j, end=" ")
+
+    print()
